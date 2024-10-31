87fb9314c0bc70e4883d14ddde83a733be15a5d2
22120163 <22120163@student.hcmus.edu.vn>
2024-10-31 19:56:35 +0700

    Data binding for selected song in search result

--- ViewModels/SongDetailViewModel.cs
+++ ViewModels/SongDetailViewModel.cs
@@ -1,11 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// SongDetailViewModel.cs
+using CommunityToolkit.Mvvm.ComponentModel;
+using Spotify.Models.DTOs;
 
 namespace Spotify.ViewModels;
 
-public class SongDetailViewModel
+public partial class SongDetailViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string title = string.Empty;
+
+    [ObservableProperty]
+    private string moreInfo = string.Empty;   
+    
+    [ObservableProperty]
+    private string lyrics = string.Empty;
+
+    [ObservableProperty]
+    private string artistInfo = string.Empty;
+
+    [ObservableProperty]
+    private string imageUrl = string.Empty;
+
+    public void Initialize(SongDTO songInfo)
+    {
+        Title = songInfo.title;
+        Lyrics = songInfo.plainLyrics;
+        MoreInfo = $"{songInfo.ArtistName} • {songInfo.ReleaseDate} • {songInfo.Duration}";
+        ArtistInfo = $"{songInfo.ArtistName}";
+        ImageUrl = songInfo.CoverArtUrl ?? "ms-appx:///Assets/Image1.jpg";
+    }
 }
