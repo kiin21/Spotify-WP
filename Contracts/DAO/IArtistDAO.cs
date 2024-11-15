90a78962594d33f6793c739a7a8c4911353fca41
22120163 <22120163@student.hcmus.edu.vn>
2024-11-15 21:38:08 +0700

    _

---
+++ Contracts/DAO/IArtistDAO.cs
@@ -0,0 +1,15 @@
+﻿// IArtistDAO.cs
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Spotify.Models.DTOs;
+
+namespace Spotify.Contracts.DAO
+{
+    public interface IArtistDAO : IDAO
+    {
+        Task<List<ArtistDTO>> GetAllArtistsAsync();
+        Task<ArtistDTO> GetArtistByIdAsync(string id);
+
+        Task<ArtistDTO> GetArtistByNameAsync(string name);
+    }
+}
---
+++ Models/DTOs/ArtistDTO.cs
@@ -0,0 +1,35 @@
+﻿// ArtistDTO.cs
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace Spotify.Models.DTOs
+{
+    public class ArtistDTO
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("biography")]
+        public string Biography { get; set; }
+
+        [BsonElement("avatar_url")]
+        public string Avatar { get; set; }
+
+        [BsonElement("country")]
+        public string Country { get; set; }
+
+        [BsonElement("debut_date")]
+        public DateTime DebutDate { get; set; }
+
+        [BsonElement("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("song_ids")]
+        public List<string> SongIds { get; set; } = new List<string>();
+    }
+}
