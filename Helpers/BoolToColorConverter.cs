2ef266fc642610cd38422e16947e1ecf92c7e51e
22120163 <22120163@student.hcmus.edu.vn>
2024-11-20 15:21:56 +0700

    Change playback control

--- Helpers/BoolToColorConverter.cs
+++ Helpers/BoolToColorConverter.cs
@@ -1,30 +1,30 @@
-﻿using Microsoft.UI;
-using Microsoft.UI.Xaml.Media;
-using System;
-using System.Globalization;
-using Microsoft.UI.Xaml.Data;
-using Windows.UI;
+﻿//using Microsoft.UI;
+//using Microsoft.UI.Xaml.Media;
+//using System;
+//using System.Globalization;
+//using Microsoft.UI.Xaml.Data;
+//using Windows.UI;
 
-namespace Spotify.Helpers
-{
-    public class BoolToColorConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            if (value is bool isChecked)
-            {
-                if (isChecked)
-                {
-                    // Red color
-                    return new SolidColorBrush(Color.FromArgb(255, 29, 185, 84));
-                }
-            }
-            return new SolidColorBrush(Colors.White);
-        }
+//namespace Spotify.Helpers
+//{
+//    public class BoolToColorConverter : IValueConverter
+//    {
+//        public object Convert(object value, Type targetType, object parameter, string language)
+//        {
+//            if (value is bool isChecked)
+//            {
+//                if (isChecked)
+//                {
+//                    // Red color
+//                    return new SolidColorBrush(Color.FromArgb(255, 29, 185, 84));
+//                }
+//            }
+//            return new SolidColorBrush(Colors.White);
+//        }
 
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+//        public object ConvertBack(object value, Type targetType, object parameter, string language)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
