b5828a8af063ac466ae4ebddfc5931d1fbbf6146
22120163 <22120163@student.hcmus.edu.vn>
2024-12-05 15:11:05 +0700

    Add playhistory DAO, DTO

---
+++ Contracts/DAO/IPlayHistoryDAO.cs
@@ -0,0 +1,13 @@
+﻿// IPlayHistoryDAO.cs
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Spotify.Models.DTOs;
+
+namespace Spotify.Contracts.DAO;
+
+public interface IPlayHistoryDAO: IDAO
+{
+    Task<List<PlayHistoryDTO>> GetUserHistoryAsync(string userID);
+}
+
+
---
+++ DAOs/PlayHistoryDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Spotify.Contracts.DAO;
+using Spotify.Models.DTOs;
+
+namespace Spotify.DAOs;
+
+public class PlayHistoryDAO : BaseDAO, IPlayHistoryDAO
+{
+    private readonly IMongoCollection<PlayHistoryDTO> _playHistory;
+    public PlayHistoryDAO()
+    {
+        var database = connection.GetDatabase("SpotifineDB");
+        _playHistory = database.GetCollection<PlayHistoryDTO>("PlayHistory");
+    }
+    public async Task<List<PlayHistoryDTO>> GetUserHistoryAsync(string userID)
+    {
+        // Find documents where UserID matches and sort by PlayedAt in descending order
+        return await _playHistory
+            .Find(playHistory => playHistory.UserID == userID)
+            .SortByDescending(playHistory => playHistory.PlayedAt)
+            .ToListAsync();
+    }
+}
+
