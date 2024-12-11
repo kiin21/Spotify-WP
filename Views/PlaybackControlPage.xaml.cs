f329c7976ff34e0cb523d284197fad9addfc832e
22120163 <22120163@student.hcmus.edu.vn>
2024-12-11 12:18:52 +0700

    small fix

--- Views/PlaybackControlPage.xaml.cs
+++ Views/PlaybackControlPage.xaml.cs
@@ -21,40 +21,4 @@ public sealed partial class PlaybackControlPage : Page
         ViewModel = PlaybackControlViewModel.Instance;
         DataContext = ViewModel;
     }
-
-    private void Slider_PointerPressed(object sender, PointerRoutedEventArgs e)
-    {
-        if (ViewModel.CurrentSong == null || ViewModel.TotalDurationSeconds <= 0)
-        {
-            Debug.WriteLine("Cannot seek: No song playing or invalid duration");
-            return;
-        }
-        ViewModel.OnSliderDragStarted();
-    }
-
-    private void Slider_PointerReleased(object sender, PointerRoutedEventArgs e)
-    {
-        if (ViewModel.CurrentSong == null || ViewModel.TotalDurationSeconds <= 0)
-        {
-            Debug.WriteLine("Cannot seek: No song playing or invalid duration");
-            return;
-        }
-
-        var slider = sender as Slider;
-        if (slider != null)
-        {
-            ViewModel.CurrentPosition = TimeSpan.FromSeconds(slider.Value);
-            ViewModel.OnSliderDragCompleted();
-        }
-    }
-
-    private void Slider_Tapped(object sender, TappedRoutedEventArgs e)
-    {
-
-    }
-
-    private void Slider_Tapped_1(object sender, TappedRoutedEventArgs e)
-    {
-
-    }
 }
